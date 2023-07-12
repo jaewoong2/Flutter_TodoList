911bd94cd01b9aeb3d079cb1af83e69ceec27889
jaewoong2 <jwisgenius@naver.com>
2023-07-13 06:20:02 +0900

    :lipstick: 앱바 구현

---
+++ lib/ui/todo_appbar_ui.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TodoAppBar extends StatelessWidget {
+  final String title;
+
+  const TodoAppBar({super.key, this.title = '해빗츠'});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: const Color(0xffffffff),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.pets_rounded,
+            color: Colors.black,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
