8c9ba726fa1091fe34d65bde3f596ae0cfe86638
jaewoong2 <jwisgenius@naver.com>
2023-07-14 20:15:51 +0900

    :art: 타입 변경에 따른 Todo MOdel 변경

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -1,12 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todolist/model/category_model.dart';
+
 class TodoModel {
   //player 중인지를 확인할 값
   late bool isCompleted = false;
   late String todo = '';
+  late CategoryModel category;
+  late Color color;
+  late List<String> schedule;
+
   late int id = 0;
 
   TodoModel({
     required this.isCompleted,
     required this.todo,
     required this.id,
+    required this.category,
+    required this.color,
+    required this.schedule,
   });
+
+  TodoModel.fromTodo(TodoModel newTodo)
+      : isCompleted = false,
+        todo = newTodo.todo,
+        category = newTodo.category,
+        color = newTodo.color,
+        schedule = newTodo.schedule;
 }
